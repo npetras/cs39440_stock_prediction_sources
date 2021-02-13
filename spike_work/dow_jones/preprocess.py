7f60bb3e4a979ad42a0e0ddabe4cf04c320293ca
Nicolas Petras <nicolaspetras@gmail.com>
2021-02-13 16:17:26 +0000

    FMP-93 spike: updates to pre-processing functions & tests This current version passes the test, but may not behave in the desired manner, this point has to be considered.

--- spike_work/dow_jones/preprocess.py
+++ spike_work/dow_jones/preprocess.py
@@ -1,8 +1,13 @@
+"""
+Module for pre-processing and cleaning data (specifically stock market), but is also suitable 
+for other data. The pre-processing is to prepare text data for sentiment analysis.
+"""
 import string
 import re
 
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
+from nltk.tokenize import treebank
 
 # does not include % or $, since those are useful in terms of stocks
 PUNCT_REGEX = """[!"#&\'()*\+,-.\/\\:;<=>?@\[\]^_`{}~\|]"""
@@ -11,24 +16,28 @@ SPACE = ' '
 BLANK = ''
 
 def to_lowercase(text):
+    """
+    Returns the 'text' as a lowercase string, by simply apply the lower() to the
+    string.
+    """
     return text.lower()
 
 def remove_punct(text):
     """
-    
+    Returns the string 'text' with its punctuation removed, this function expects
+    'text' to be a string.
     """
     return text.translate(str.maketrans('', '', string.punctuation))
     
 def remove_stopwords(text):
     """
-
     """
     stop_words = stopwords.words('english')
-    words = text.word_tokenize()
+    words = word_tokenize(text)
     # regex for stopwords that should not be removed from text, since they have negative sentiment
     whitelist = [".*n't", 'not', 'no']
     modified_stop_words = []
-    processed_text = []
+    processed_text_list = []
 
     # create a new list of stop words without those on the white list
     for stop_word in stop_words:
@@ -37,14 +46,17 @@ def remove_stopwords(text):
         else:
             modified_stop_words.append(stop_word)
     
-    processed_text = [word for word in words if (word not in modified_stop_words)]
+    processed_text_list = [word for word in words if (word not in modified_stop_words)]
 
-    return processed_text
+    return treebank.TreebankWordDetokenizer().detokenize(processed_text_list)
 
 def lemmatization(text):
     """
     """
     pass
+
+def apply_all(text):
+    processed_text = to_lowercase(text)
 
 # read in file
 
--- spike_work/dow_jones/test_pre_processing.py
+++ spike_work/dow_jones/test_pre_processing.py
@@ -1,18 +1,49 @@
 import unittest
-import pre_processing as pre_process
+import preprocess
 
 class TestPreprocessing(unittest.TestCase):
-    def test_punct_basic(self):
+    def test_to_lowercase_1(self):
+        text = "HELLO HOW ARE YOU?"
+        expected_text = "hello how are you?"
+        result = preprocess.to_lowercase(text)
+        self.assertEqual(result, expected_text)
+    
+    def test_to_lowecase_2(self):
+        text = "HELLO how are YOU?"
+        expected_text = "hello how are you?"
+        result = preprocess.to_lowercase(text)
+        self.assertEqual(result, expected_text)
+    
+    def test_punct_basic_string(self):
         text = """Hello how are you doing today?!"""
         expected_text = "Hello how are you doing today"
-        result = pre_process.remove_punct(text)
+        result = preprocess.remove_punct(text)
         self.assertEqual(result, expected_text)
 
-    def test_punct_advanced(self):
-        text = """Hello how's are you #twitter doing (fifty) '\n' today?!"""
+    def test_punct_complex_string(self):
+        text = """Hello how's are you #twitter doing (fifty) '{n}' today?!"""
         expected_text = "Hello hows are you twitter doing fifty n today"
-        result = pre_process.remove_punct(text)
+        result = preprocess.remove_punct(text)
         self.assertEqual(result, expected_text)
+    
+    def test_remove_stopwords_basic_stopwords(self):
+        text  = "georgia 'downs two russian warplanes' as countries move to brink of war"
+        expected_text = "georgia 'downs two russian warplanes' countries move brink war"
+        result = preprocess.remove_stopwords(text)
+        self.assertEqual(result, expected_text)
+
+    def test_remove_stopwords_basic_stopwords2(self):
+        text  = """I Know We've Got This Election in US To Worry, But I Wanted To Help Shine More Light On Our Australian Friends Potential 'Internet Filter' Problem. Let's Keep That In The Spotlight Too!""".lower()
+        expected_text = "Know've Got Election US Worry, Wanted Help Shine Light Australian Friends Potential 'Internet Filter' Problem . Let's Keep Spotlight!".lower()
+        result = preprocess.remove_stopwords(text)
+        self.assertEqual(result, expected_text)
+
+    def test_remove_stopwords_no_punct_string(self):
+        text  = """I Know We ve Got This Election in US To Worry, But I Wanted To Help Shine More Light On Our Australian Friends Potential 'Internet Filter' Problem. Let's Keep That In The Spotlight Too!""".lower()
+
+
+    def test_remove_stopwords_whitelist_words(self):
+        pass
 
 if __name__=='__main__':
     unittest.main()
