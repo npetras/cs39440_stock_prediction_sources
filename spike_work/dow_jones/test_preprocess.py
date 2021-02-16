b493a477b0c733fcc4673b1e18bcfcb4b19d0b2e
Nicolas Petras <nicolaspetras@gmail.com>
2021-02-16 16:16:57 +0000

    FMP-93 spike: tested apply_all function, made some other minor updates Updated name, added to_lowercase headline test,

--- spike_work/dow_jones/test_preprocess.py
+++ spike_work/dow_jones/test_preprocess.py
@@ -1,7 +1,8 @@
 import unittest
 import preprocess
 
-US_ELECTION_HEADLINE_RAW_LC = """I Know We've Got This Election in US To Worry, But I Wanted To Help Shine More Light On Our Australian Friends Potential 'Internet Filter' Problem. Let's Keep That In The Spotlight Too!""".lower()
+US_ELECTION_HEADLINE_RAW = """I Know We've Got This Election in US To Worry, But I Wanted To Help Shine More Light On Our Australian Friends Potential 'Internet Filter' Problem. Let's Keep That In The Spotlight Too!"""
+US_ELECTION_HEADLINE_RAW_LC = US_ELECTION_HEADLINE_RAW.lower()
 
 
 class TestPreprocessing(unittest.TestCase):
@@ -15,6 +16,12 @@ class TestPreprocessing(unittest.TestCase):
     def test_to_lowecase_mixed_caps(self):
         text = "HELLO how are YOU?"
         expected_text = "hello how are you?"
+        result = preprocess.to_lowercase(text)
+        self.assertEqual(result, expected_text)
+    
+    def test_to_lowercase_headline(self):
+        text = US_ELECTION_HEADLINE_RAW
+        expected_text = US_ELECTION_HEADLINE_RAW_LC
         result = preprocess.to_lowercase(text)
         self.assertEqual(result, expected_text)
     
@@ -76,16 +83,17 @@ class TestPreprocessing(unittest.TestCase):
         result = preprocess.lemmatize_text(text)
         self.assertEqual(result, expected_text)
                 
-    def test_apply_all_1(self):
-        text  = """I Know We've Got This Election in US To Worry, But I Wanted To Help Shine More Light On Our Australian Friends Potential 'Internet Filter' Problem. Let's Keep That In The Spotlight Too!"""
-        expected_text  = """I Know We've Got This Election in US To Worry, But I Wanted To Help Shine More Light On Our Australian Friends Potential 'Internet Filter' Problem. Let's Keep That In The Spotlight Too!""".lower()
-        # result = preprocess.apply_all(text)
-        # self.assertEqual(result, expected_text)
+    def test_apply_all_short_headline(self):
+        text  = """Georgia 'downs two Russian warplanes' as countries move to brink of war"""
+        expected_text  = "georgia down two russian warplane country move brink war"
+        result = preprocess.apply_all(text)
+        self.assertEqual(result, expected_text)
 
-    def test_apply_all_2(self):
-        text  = """ """
-        expected_text  = """ """.lower()
-        # result = preprocess.apply_all(text)
-        # self.assertEqual(result, expected_text)
+    def test_apply_all_long_headline(self):
+        text  = US_ELECTION_HEADLINE_RAW_LC
+        expected_text  = "know get election us worry want help shine light australian friend potential internet filter problem let keep spotlight"
+        result = preprocess.apply_all(text)
+        self.assertEqual(result, expected_text)
+
 if __name__=='__main__':
     unittest.main()
