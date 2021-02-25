6f1f5fc89d3e32ba5c7d47d54af09d1d95776da0
Nicolas Petras <nicolaspetras@gmail.com>
2021-02-25 19:51:57 +0000

    FMP-131: feat, refactor: added naive bayes classification Classified data using sklearn's count vectorizer and naive bayes cleaned up the code, adding a function for combining each row of headlines

--- spike_work/personal/dow_jones/constants.py
+++ spike_work/personal/dow_jones/constants.py
@@ -1,1 +1,2 @@
-PATH_TO_DATASET = '../../../datasets/existing/dow_jones/Combined_News_DJIA.csv'
+REL_PATH_TO_DATASET = '../../../datasets/existing/dow_jones/Combined_News_DJIA.csv'
+ABS_PATH_TO_DATASET = '/Users/nicolaspetras/workspace/university/semester_2/cs39440_mmp/project_repo/cs39440_stock_prediction_sources/datasets/existing/dow_jones/Combined_News_DJIA.csv'
--- spike_work/personal/dow_jones/naive_bayes.py
+++ spike_work/personal/dow_jones/naive_bayes.py
@@ -1,8 +1,26 @@
 import pandas as pd
-import nltk.classify.scikitlearn
+from nltk.tokenize.regexp import WordPunctTokenizer
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.naive_bayes import MultinomialNB
+from nltk.classify import scikitlearn
+
 
 from preprocessing import preprocess
 import constants
+
+def combine_headline_rows(data_frame):
+    combined_headlines = []
+
+    for row in range(0, len(data_frame.index)):
+        combined_headlines_in_row = ''
+        for headline in data_frame.iloc[row, 2:27]:
+            headline_str = str(headline)
+            combined_headlines_in_row = combined_headlines_in_row + ' ' + headline_str
+        # remove dangling spaces at beginning and end, trim?
+        combined_headlines.append(combined_headlines_in_row.strip())
+
+    return combined_headlines
+
 
 def get_data_for_model(tokens_list):
     for tokens in tokens_list:
@@ -10,28 +28,54 @@ def get_data_for_model(tokens_list):
 
 
 # import and split data
-data_frame = pd.read_csv(constants.PATH_TO_DATASET)
-train_df = data_frame[data_frame['Date'] < '2015-01-01']
-test_df = data_frame[data_frame['Date'] > '2014-12-31']
+djia_df= pd.read_csv(constants.ABS_PATH_TO_DATASET)
+train_df = djia_df[djia_df['Date'] < '2015-01-01']
+test_df = djia_df[djia_df['Date'] > '2014-12-31']
 
 # basic preprocessing that is required
 
 # combine each row into one string
-train_headlines_data = []
+train_headlines_data = combine_headline_rows(train_df)
+print("Train Headlines:")
+print(train_headlines_data[0] + '\n')
 
-for row in range(0, len(train_df.index)):
-    combined_headlines_in_row = ''
-    for headline in train_df.iloc[row, 2:27]:
-        headline_str = str(headline)
-        # removes bs and start and end quotes from headlines
-        headline_str = preprocess.remove_b(headline_str)
-        headline_str = preprocess.remove_start_end_quotes(headline_str)
-        combined_headlines_in_row = combined_headlines_in_row + ' ' + headline_str
+test_headlines_data = combine_headline_rows(test_df)
+print("Test Headlines:")
+print(test_headlines_data[0] + '\n')
 
-    # remove dangling spaces at beginning and end, trim?
-    train_headlines_data.append(combined_headlines_in_row.strip())
+# count vectorizer, sklearn approach
+# feature extraction
+count_vectorizer = CountVectorizer()
+vectorized_train_data = count_vectorizer.fit_transform(train_headlines_data)
+print("Sklearn Vectorizer Shape: ")
+print(f'{vectorized_train_data.shape}\n')
 
-print(train_headlines_data[0])
+# training model
+naive_bayes_classifier = MultinomialNB()
+naive_bayes_model = naive_bayes_classifier.fit(vectorized_train_data, train_df['Label'])
 
-# train model on data with no preprocessing
+# test/evaluation
+vectorized_test_data = count_vectorizer.transform(test_headlines_data)
+prediction_score = naive_bayes_model.score(vectorized_test_data, test_df['Label'])
+print(f"Sklearn MultinomiaNB Accuracy: {prediction_score}\n")
 
+# nltk manual approach
+tokenized_train_headlines = []
+
+for headlines in train_headlines_data:
+    tokenized_headlines = WordPunctTokenizer().tokenize(headlines)
+    tokenized_train_headlines.append(tokenized_headlines)
+
+print("NLTK: Tokenized Train Data: ")
+print(f'{tokenized_train_headlines[0]}\n')
+
+train_data_dict_list = get_data_for_model(tokenized_train_headlines)
+
+positive_dataset = [(train_data_dict_list, train_df['Label'])
+                         for train_data_dict in tokenized_train_headlines]
+
+train_data_for_model = []
+
+for index in enumerate(train_data_dict_list):
+    dict_label_tuple = (train_data_dict_list[index], train_df['Label'][index]
+    train_data_for_model.append(dict_label_tuple)
